02df1d67c1b3b32d5b542d474c4b3d7582d25e80
Yash <yjain8958372013@gmail.com>
2022-06-03 17:06:05 +0530

    first commit

---
+++ PHP task/README!!.txt
@@ -0,0 +1,11 @@
+Task:
+
+- Import data from data.php file 
+- Use that data to make a dynamic Blog page (like <title><?php echo $data["title"] ?></title>)
+- Content should not be hard coded, define it in data.php file if missing and use it
+- The links in table of contents should be dynamic and linked to sections (make a php script to extract h2 and h3 from blog content and link them respectivly)
+- The design is provided in the figma link
+- The UI should be responsive
+- use only css or scss for styling
+
+Figma Link (Import assets from here): https://www.figma.com/file/uqty4G6dyhSDcfKke4Gr0F/Task?node-id=0%3A1
---
+++ PHP task/data.php
@@ -0,0 +1,243 @@
+<?php
+return [    
+  //meta
+  'title'       => 'Ecommerce phone number – Quicktalk.com',
+  'description' => 'Start manage your customers calls with a super simple web based business phone system available instanly. Only $19 per month for 3 users - Unlimited calls within 36 countries',
+  'enUrl'         => 'blog/ecommerce-phone-number',
+  //page
+  'h1'            => 'Should You Get a Phone Number for Your Ecommerce Business?',
+  'date'           => 'November 8, 2021',
+  //author
+  'author_picture' => "<img height='100%' width='100%' loading='lazy' src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.insider.com%2F57ce87acdd089588528b46e4%3Fwidth%3D1000%26format%3Djpeg%26auto%3Dwebp&imgrefurl=https%3A%2F%2Fwww.businessinsider.com%2Fthe-highest-paid-authors-in-the-world-in-2016-2016-9&tbnid=thLdQkJxDUolaM&vet=12ahUKEwiuqefCspD4AhVTmtgFHdkRACgQMygGegUIARDhAQ..i&docid=jBDY3K_ppAxeKM&w=987&h=740&q=authors&ved=2ahUKEwiuqefCspD4AhVTmtgFHdkRACgQMygGegUIARDhAQ' alt='Paul Revellat' />",
+  'author_name'     => 'Paul Revellat',
+  'author_position' => '',
+  'author_note'     => 'Paul is a tech and business phone system enthusiast always open to discuss ideas. If you’re interested in collaborating on content projects or getting a product listed on our blog, don’t hesitate and reach out to us: paul@quicktalk.com.',
+  //cover photo
+  'cover'         => "<img height='100%' width='100%' loading='lazy' src='/images/cover.png' alt='Should You Get a Phone Number for Your Ecommerce Business' />",
+  //text  
+  'reading_time' => '9',
+  'table-of-content' => 'Table of content',
+  //blog content
+  'intro'         => "  
+  <p>My partner and I have been running an ecommerce business for over 10 years now, and the phone number dilemma has always been one of the most difficult issues to solve.</p><br>
+
+<p>Recently, I read a really interesting article on the topic. Back in 2013, Andrew Youderian shared his own experience of <a href='https://www.ecommercefuel.com/store-phone-number/'>dealing with phone support as a ecommerce business manager</a>. His conclusion is pretty straightforward. Do it well, or don’t do it at all. Period. </p><br>
+
+<p>Do you really need a phone number for your online business?</p><br>
+
+<p>We’ll see in this article how phone support is not necessarily a cost-effective approach, but definitely a revenue-driven approach. </p><br>
+ 
+    ",
+  'content'       => "
+  <h2>What’s at stake?</h2>
+
+
+<p>Many things have changed since 2013. Indeed, <b>countless new customer service channels have come into existence and improved</b>, such as chatbots, virtual phone systems, social media messengers, etc. Which makes the question, “Should I get a phone number for my Ecommerce Business?” a bit tricker to tackle.</p><br>
+
+
+<p>(PS. The good news is that we’ve been working so hard on a new tool that should make your life a looot easier when it comes to managing your customer service. But I’ll come to that later.)</p><br>
+
+
+<p>Here is why providing phone support for your ecommerce website is a real dilemma:</p><br>
+
+
+<p>On one hand, <b>phone support is the best way to provide high-level customer service</b> for your business, get closer to your ideal customers, and increase your online sales. On the other hand, <b>too many customer calls can be overwhelming</b>. Even worse, if you can’t answer them all, and answer them in the right way, it can even put your company in a negative light.</p><br>
+
+
+<p>So, should you or should you not?</p><br>
+
+<div style='padding-top:56.000%;position:relative;'><img height='100%' width='100%' loading='lazy' src='https://i.gifer.com/7qu0.gif' width='100%' height='100%' style='position:absolute;top:0;left:0;' alt='I don’t know what to do gif' /></div><p><a target='_blank' href='https://gifer.com'>via GIFER</a>
+​​
+
+<br>
+<p>When you start your ecommerce business, you want to make sure everything is perfectly streamlined. You want to ensure you provide the best customer experience, starting from the first visit on your website until the product delivery, customer review, and repeated business.</p><br>
+
+
+<p>Things aren’t that easy.</p><br>
+
+
+<p>Someone could have a problem making a purchase because of a small bug on your website.</p><br>
+
+
+<p>Maybe some others would want additional information about your product before taking the leap.</p><br>
+
+
+<p>Maybe some people don’t trust your website enough to put their credit card information.</p><br>
+
+
+<p>There are a thousand reasons for a prospect not to convert on your website. </p><br>
+
+
+<p>But those barriers shouldn’t make you lose business. In most cases, if they could have a quick call with you or someone on your team, the issue could be solved and you could make an extra sale.</p><br>
+
+
+<p>If you don’t have a phone number that people can reach out to on your website, you will surely miss some sales. 100%. </p><br>
+
+
+<p>The real question is, are those lost sales worth the investment of providing phone support?</p><br>
+
+  <h2>5 benefits of providing phone support for your ecommerce business</h2>
+  
+  <h3>Offer human customer service</h3>
+  
+  <p>Back in the day, the only way to ask a question to a business was to call them. Things have changed a lot. Now, you can use phone, contact form, email, chatbot, live chat, social media accounts, messenger apps… and probably many more to come in the future!</p><br>
+  
+  
+  <p>Among them all, <b>the phone channel is special because it’s truly about direct human-to-human interactions</b>. Many of the other channels above will help businesses save time and money, mainly because these channels can be automated like chatbots, and also because there is a delay between the request and the expected answer. </p><br>
+  <picture>
+    <source type='image/webp' srcset='/images/human.desktop.webp' media='(min-width: 800px)'>
+    <source type='image/webp' srcset='/images/human.mobile.webp' media='(max-width: 800px)'>
+    <img srcset='/images/human.desktop.jpg 1850w,
+                /images/human.mobile.jpg 1350w' src='/images/human.desktop.jpg' alt='human to human interactions' width='100%' height='100%' loading='lazy'>
+  </picture>
+
+  <br>
+  <p>Opposed to a phone call, you don’t have to respond to a contact form request within seconds. It’s way easier and cheaper to manage.</p><br>
+  
+  
+  <p>Indeed, that’s what most customers prefer. A survey by American Express found <a target='_blank' href='http://about.americanexpress.com/news/docs/2012x/axp_2012gcsb_us.pdf'>that 46% of customers prefer to speak about issues by phone</a>. It’s faster from a customer point of view, that’s all. </p><br>
+  
+  
+  <p>As a customer, if you are having an issue on someone’s website, you can spend half an hour reading the FAQ section or you can write a detailed email about your issue and pray for someone to read it. However, it could have all been solved instantly with a phone call.</p><br>
+  
+  
+  <p><b>Phone calls solve problems better, faster and with a touch of empathy</b>. That’s what all valuable customers deserve.</p><br>
+  
+  
+  <p>In that sense, implementing the phone channel for your business increases customer satisfaction. And happy customers means repeat business!</p><br>
+  
+  
+  
+  <h3>Increase trust, increase conversion</h3>
+  
+  <p>The main goal for ecommerce businesses is to turn casual web browsers into life-long repeat customers. Unlike the customer path we dream of, the road to converting an online visitor on your website is long and winding, believe me.</p><br>
+  
+  <picture>
+    <source type='image/webp' srcset='/images/expectations.desktop.webp' media='(min-width: 800px)'>
+    <source type='image/webp' srcset='/images/expectations.mobile.webp' media='(max-width: 800px)'>
+    <img srcset='/images/expectations.desktop.jpg 1850w,
+                /images/expectations.mobile.jpg 1350w' src='/images/expectations.desktop.jpg' alt='expectation vs reality drawing' width='40%' height='100%' loading='lazy'>
+  </picture>
+  <br>
+  <p>Here is your expectation of the customer journey:<br>
+  
+  <ul><li>Customers find you on Google or Instagram</li>
+  <li>Browse your products catalog</li>
+  <li>Add to cart</li>
+  <li>Check out</li>
+  <li>Nurture and repeat business</li></ul></p><br>
+  
+  <p>Here’s what probably happens in real life:
+  <ul><li>Customers might find your website, and at the same time, open a dozen of competitors websites</li>
+  <li>They seek for customer reviews all over the web</li>
+  <li>They do additional web research browsing videos, social media, forums, etc.</li>
+  <li>They change their mind and go in the street to buy your product in a real store</li>
+  <li>They just change their mind and decide not to buy any product at all</li>
+  <li>They go on your website once a month for a year and still hesitate whether they really take the leap</li>
+  <li>At some point on this winding journey, they might add to cart and after some other steps put their credit card to check out.</li></ul></p><br>
+  
+  <p>Having a phone number that customers can easily call you on will help them make the step forward in their decision making process. From the point of view of the customer, a good, warm conversation with a human is worth a thousand web researches. </p><br>
+  
+  <p>In the end, <b>web conversion is all about trust. And trust is all about human-to-human interactions</b>. To put it in other words, trust is about conversations. </p><br>
+  
+  
+  <h3>Learn faster by learning from your customer</h3>
+  
+  <p>Especially when you start off an ecommerce website, having the chance to talk directly with your first customers is gold. </p><br>
+  
+  <p>How did they find your website? What makes them hesitate? What went wrong during their visit? What did they like about your products? Will they buy on your website again? What could be improved? </p><br>
+  
+  <p>There are many ways to gather insights out there. For instance, web analytics, A/B testing and CRO (Conversion Rate Optimisation) can tell a lot about the experience you deliver on your website. </p><br>
+  
+  <p><i>Want to know more about CRO? Check out this insightful article about <a target='_blank' href='https://www.hotjar.com/conversion-rate-optimization/'>how to optimise your conversion rate from Hotjar</a>.</i></p><br>
+  
+  <p>Figures and statistics bring you a lot. They’ll help you make tweaks here and there on your website to increase online performance. </p><br>
+  
+  <p>But <b>in-depth feedback can only come with a conversation</b>. And it’s the most precious kind of feedback. Because it comes with context and details. And also because it comes with feeling and emotion.</p><br>
+  
+  <p>As orders increase on your website, you will learn a lot from your customers in many ways. Giving your first customers the opportunity to call you or your team will help you learn faster. Be close to them, talk to them. </p><br>
+  
+  <p>Having an online business doesn’t mean you shouldn’t build relationships with your customers. Quite the contrary, competition is higher on the web. Creating bonds with your customers is the only way to turn a one-time buyer into a life-long repeat customer. </p><br>
+  
+  <p>A good metaphor is the entrepreneur that creates inventions by spending his days alone in his basement. Unless he is a genius, there are poor chances that he reaches a good product-market fit. Instead the right thing to do is to go out into the world and learn from people while building products for them. </p><br>
+  
+  
+  <h3>Improve your brand image</h3>
+  
+  <p>Customer service and brand image are two sides of the same coin. That’s one thing companies tend to forget when it comes to building their brands, Anand Subramaniam says in <a target='_blank' href='https://hbr.org/2009/04/six-ways-to-build-your-brand-t'>a Harvard Business Review article</a> from 2009: </p><br>
+  
+  <p><i>“When it comes to brand building, customer service is often the last and most-ignored piece of the puzzle”.</i></p><br>
+  
+  <p>And when it comes to small brands - I’m sure you are really good at branding, but you wouldn’t be reading this article if your brand was Coca-Cola or Apple - <b>the most trustable brands are the ones that you can talk to</b>. That’s what makes it real and grows engagement with your audience.</p><br>
+  
+  <p>Building your brand is all about delivering positive experiences to your audience. And that’s what the phone does better than any other channel when it comes to answering a question or solving an issue.</p><br>
+  
+  <h3>Create more upsell opportunities</h3>
+  
+  <p>I highly recommend every marketer to read <a target='_blank' href='https://books.google.fr/books/about/Marketing_Metrics.html?id=7Ptw4nBoGmkC&redir_esc=y'>Marketing Metrics: The Definitive Guide to Measuring Marketing Performance</a>. Here what it says about upsell:</p><br>
+  
+  <i>“The probability of selling to a new prospect is 5-20%. The probability of selling to an existing customer is 60-70%.”</i></p><br>
+    
+  <picture>
+    <source type='image/webp' srcset='/images/shopping.desktop.webp' media='(min-width: 800px)'>
+    <source type='image/webp' srcset='/images/shopping.mobile.webp' media='(max-width: 800px)'>
+    <img srcset='/images/shopping.desktop.jpg 1850w,
+                /images/shopping.mobile.jpg 1350w' src='/images/shopping.desktop.jpg' alt='shopping cart inspirational picture' width='100%' height='100%' loading='lazy'>
+  </picture>
+  
+  <br><p>When a customer calls you on the phone, it’s a clear opportunity to point out other items that they could be interested in. You can also take the time to explain the special offers and discounts that can lead to more sales. It’s all about the sales pitch. If you train your agent that way, each phone call you receive will potentially turn into more business.  </p><br>
+  
+  
+  <h2>3 common issues with phone support and how to fix them</h2>
+  
+  <p>We’re not going to lie here. Implementing phone support for your ecommerce business is not just about putting a phone number on your home page.</p><br>
+  
+  <h3>Phone support can be costly</h3>
+  
+  <p>From a staffing perspective, <b>you need to have people answering customer calls</b>. At the beginning, one ecommerce manager can answer customer calls himself. As we said earlier it’s a very good practice to learn fast from your first customers. </p><br>
+  
+  <p>As you grow, you need to have dedicated members of your team doing the job. And they need to be well trained. You shouldn’t underestimate that cost. </p><br>
+  
+  <p>But in the meantime, if you handle it well, there are a lot of ways to optimize that cost. Mainly with technology.</p><br>
+  
+  <p>Here’s a few tips on how to optimize the cost of your phone support: 
+  <ul>
+    <li>Improve your Interactive Voice Response menu: The ultimate goal is to shorten the average response time of customer calls. Here’s <a target='_blank' href='https://www.ringover.com/blog/practical-guide-get-started-phone-system'>a few tips for an amazing IVR setup</a>.</li>
+    <li>Limit phone hours: You can’t answer calls 24/7 and for most businesses you don’t have to. Make it clear on your website and in your custom greetings. Letting customers call your business on a relevant schedule will lower your customer service costs and still allows you to provide a great service.</li>
+    <li>Display your phone number in strategic spots only: You don’t want your phone system to be overwhelmed with low-value calls. Some calls will make your business lose time and money. So place your phone number on pages that only high-value visitors will have access to, such as:
+      <ul>
+        <li>The cart checkout page</li>
+        <li>Product pages that bring you more profit</li>
+        <li>A pop-up window that pops when someone has viewed 3 FAQ pages
+        <li>Etc.</li>
+      </ul>
+    </li>
+  </ul>
+  </p><br>
+  
+  <h3>If calls are handled badly, it’s worse than not having a number at all</h3>
+  
+  <p>We all know the feeling of being stuck on the phone waiting for someone to answer for hours when calling a customer service number (it certainly lasts a few minutes, but it feels like hours). People hate this feeling. If you put them in that position, they could start to get frustrated with your brand.</p><br>
+  
+  <p>That’s what you want to avoid. If you apply the tips from the <a target='_blank' href='https://www.ringover.com/blog/practical-guide-get-started-phone-system'>Ringover article</a> above, you should be fine. </p><br>
+  
+  <p>In any case, <b>a good phone system should help you easily monitor and improve your IVR</b> on a weekly basis. If you set up one and realize your average waiting time is over 5 minutes, make improvements quickly or forget it. </p><br>
+  
+  <h3>Few business phone systems really fit ecommerce business’s needs</h3>
+  
+  <p>If you look at the main players in the market, the average cost of a business phone system for a small business is around 25-30$ / month / licence. Ringcentral standard plan costs 27$ per users, Grasshopper stands at 29$ per users, Vonage is 20$ per users… And most of those solutions have hidden costs - calls are not always unlimited, and international calls cost a fortune.</p><br>
+  
+  <p>Truly what a business needs is not just a phone number with minutes of communication. They  need to be able to:
+  <ul><li>Set up custom greetings with an interactive voice menu</li>
+  <li>Configure call redirections</li>
+  <li>See detailed call logs, including who picked up the phone, how long it lasted, the call recording, etc.</li>
+  <li>Browse analytics about your phone activity for constant improvement</li></ul></p><br>
+  
+  <h2>To sum it up</h2>
+  
+  <p>Making the decision to get a phone number for your ecommerce business is a big step. It can be stressful deciding if it’s something your business can handle or not. But ultimately, <b>adding a business phone line greatly increases conversion, inspires confidence, and increases customer loyalty</b>, all while giving your business critical insights about how to sell better.</p><br>
+  
+  <p>If you are not sure whether opening a business phone line is right for you, the best idea may be to try it out and see the results it could have for your business. See here how you can get started quickly and easily.</p>
+    
+  "
+];
